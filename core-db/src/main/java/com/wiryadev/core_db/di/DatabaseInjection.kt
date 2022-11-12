4843300e6d82fdc9540ffe404c44f5222dece0fa
wiryadev <abrarwiryawan@gmail.com>
2022-11-12 13:27:25 +0700

    Initial commit

---
+++ core-db/src/main/java/com/wiryadev/core_db/di/DatabaseInjection.kt
@@ -0,0 +1,12 @@
+package com.wiryadev.core_db.di
+
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+
+object DatabaseInjection {
+//    fun provideDatabase = AndroidSqliteDriver(
+//        schema = AppDatabase.Scheme,
+//        context = context,
+//        name = "app.db"
+//    )
+}
---
+++ data-foo/src/main/java/com/wiryadev/data_foo/DataFooInjection.kt
@@ -0,0 +1,5 @@
+package com.wiryadev.data_foo
+
+
+object DataFooInjection {
+}
